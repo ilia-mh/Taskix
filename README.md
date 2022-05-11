d5abe6225d66db024b52fccfcdc8ad177bc8e173
Amir Mohseni <iliya.numb@gmail.com>
2022-05-12 01:03:02 +0200

    seperated active and completed todos

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A simple task manager built with React and TypeScript to add your daily tasks.
 Combination of useReducer and useContext is used in the project for todos. Its built scalable so you can
 add new reducers and states in Store.tsx file.
 
-## TODOS
+## Project TODOS
+- [x] Seperate Completed Todos from Active Todos
 - [ ] Make Todos draggable to move them up and down
 - [ ] Write Unit Tests for TodosContext
 - [ ] Add Cypress and write E2E tests for the todos
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,38 @@
-import React, { useContext } from 'react'
-import SingleTodo from './singleTodo';
+import React, { useContext } from "react";
+import SingleTodo from "./singleTodo";
 
 import { Store } from "../store/Store";
+import { Todo } from "../model";
 
 const TodoList: React.FC = () => {
+  const { todos } = useContext(Store);
 
-  const { todos } = useContext(Store)
+  const getTodos: (isDone: boolean) => Todo[] = (isDone: boolean) =>
+    todos.filter((todo) => todo.isDone == isDone);
 
   return (
-    <div className='todos'>
-      {
-        todos.length ?
-          todos.map( todo => 
-            <SingleTodo todo={todo} key={todo.id} />
-          )
-        :
-        'No Todos'
-      }
-    </div>
-  )
-}
+    <div className="container">
 
-export default TodoList
+      <div className="todos">
+
+        <span className="todos__heading">Active Tasks</span>
+
+        {
+          getTodos(false).map( todo => <SingleTodo todo={todo} /> )
+        }
+
+      </div>
+
+      <div className="todos remove">
+        <span className="todos__heading">Completed Tasks</span>
+
+        {
+          getTodos(true).map( todo => <SingleTodo todo={todo} /> )
+        }
+      </div>
+
+    </div>
+  );
+};
+
+export default TodoList;
